2340a85260bf5512a58c836d48c0f85ce03bae46
Devpoa Software <suporte.devpoa@gmail.com>
2022-02-27 13:19:54 -0300

    Flutter: Criando primeiras telas do app pet life

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lifepet_app/screens/home_screen.dart';
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  const Main({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pet Life",
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/models/pet_model.dart
@@ -0,0 +1,44 @@
+class Pet {
+  String nome;
+  String imageUrl;
+  String descricao;
+  int idade;
+  String sexo;
+  String cor;
+  String bio;
+  String id;
+
+  Pet({
+    required this.nome,
+    required this.imageUrl,
+    required this.descricao,
+    required this.idade,
+    required this.sexo,
+    required this.cor,
+    required this.bio,
+    required this.id
+  });
+}
+
+var pets = [
+  Pet(
+    nome: "Totó",
+    imageUrl: 'assets/images/toto.jpg',
+    descricao: "Um cachorro esperto",
+    idade: 2,
+    sexo: "Macho",
+    cor: "Marrom",
+    bio: "Sou um cachorro bem esperto",
+    id: "1"
+  ),
+  Pet(
+    nome: "Arnaldo",
+    imageUrl: 'assets/images/arnaldo.jpg',
+    descricao: "Um cachorro elétrico",
+    idade: 3,
+    sexo: "Macho",
+    cor: "Preto",
+    bio: "Sou um cachorro bem esperto e elétrico",
+    id: "2"
+  ),
+];
